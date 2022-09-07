0ac841baf1bfe91705657e966f8581d6b8b7daf2
Eithliu <iris.benoit@gmail.com>
2022-09-07 21:17:17 +0200

    new logo

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img alt="Vue logo" src="./assets/chucky.png">
     <HelloWorld msg="Chucky - Les citations"/>
   </div>
 </template>
