8c1854844e8f96d3dccbe2352eba2bb3b7900097
Eithliu <iris.benoit@gmail.com>
2022-09-07 21:05:08 +0200

    mise en place

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Chucky - Les citations"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h3>{{ info.joke }}</h3>
+    <button class="my-btn">Une autre citation !</button>
+  </div>
+</template>
+
+<script>
+const axios = require('axios');
+axios.defaults.headers.get['Accepts'] = 'application/json';
+axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+axios.defaults.headers.common['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept';
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      info: null,
+    }
+  },
+  mounted() {
+    axios
+      .get('https://chuckn.neant.be/api/rand')
+      .then(response => (this.info = response))
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+button.my-btn {
+  background-color: #607196;
+  color: #fff;
+  border: none;
+  padding: 1rem 3rem;
+}
+button.my-btn:hover {
+  background-color: #425376;
+  color: #fff;
+  border: none;
+  padding: 1rem 3rem;
+  cursor: pointer;
+}
+</style>
